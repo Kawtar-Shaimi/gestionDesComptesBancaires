a83ea1ece4a728526755abcf0119c210d30beb43
Kawtar <Kawtar.shaimi8@gmail.com>
2025-09-20 16:51:19 +0100

    BANK-7: Création de la classe Retrait

---
+++ src/model/Retrait.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Retrait extends Operation {
+    private final String destination;
+
+    public Retrait(double montant, String destination) {
+        super(montant);
+        this.destination = destination == null ? "Inconnu" : destination;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+}
